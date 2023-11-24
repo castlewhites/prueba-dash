dbbf62334d95105f64db3830d2c34daee0079816
SEBAS <scastiblanco2003@gmail.com>
2023-11-24 14:35:28 -0500

    pueba1

--- layout/callbacks.py
+++ layout/callbacks.py
@@ -3,7 +3,6 @@ from dash.dependencies import Input, Output, State
 from dash import html
 from login import login_layout
 from dashboard import dashboard_layout
-from pagina import pagina_layout
 
 def register_callbacks(app):
     @app.callback(
@@ -11,33 +10,24 @@ def register_callbacks(app):
          Output("login-output", "children"),
          Output('url', 'pathname')],
         [Input("login-button", "n_clicks")],
-        [State("username", "value"), State("password", "value"),
-         State('url', 'pathname')] 
+        [State("username", "value"), State("password", "value")]
     )
-    def display_content(n_clicks, username, password, pathname):
-        if dash.callback_context.triggered_id is None:
-            # La callback se llama por primera vez
-            return login_layout, None, '/'
-        
-        # La callback fue desencadenada por algún evento
-        triggered_component = dash.callback_context.triggered_id.split('.')[0]
-
-        if triggered_component == 'url':
-            # La callback fue desencadenada por el cambio de la URL
-            if pathname == '/dashboard':
-                return dashboard_layout, None, '/dashboard'
-            elif pathname == '/page1':
-                return pagina_layout, None, '/pagina1'
-            elif pathname == '/page2':
-                return html.Div("Contenido de la Página 2"), None, '/page2'
-            else:
-                return login_layout, None, '/'
-        elif triggered_component == 'login-button':
-
+    def display_dashboard(n_clicks, username, password):
+        if n_clicks > 0:
             if username == "usuario" and password == "123":
                 return dashboard_layout, html.Div("Login exitoso", style={'color': 'green'}), '/dashboard'
             else:
                 return login_layout, html.Div("Credenciales incorrectas", style={'color': 'red'}), '/'
         else:
-    
             return login_layout, None, '/'
+
+
+    def display_page(pathname):
+        if pathname == '/dashboard':
+            return dashboard_layout
+        elif pathname == '/page1':
+            return html.Div("Contenido de la Página 1")
+        elif pathname == '/page2':
+            return html.Div("Contenido de la Página 2")
+        else:
+            return login_layout
